808a8d66f6d496352a683e906ae9af23db96ff60
ing.dacastillop <ing.dacastillop@gmail.com>
2024-09-03 15:46:08 -0500

    :bug: Fix error for control version in iOS

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -21,9 +21,11 @@ public class AppReviewPlugin: CAPPlugin {
 
 extension SKStoreReviewController {
     public static func requestReviewInCurrentScene() {
-        if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
-            DispatchQueue.main.async {
-                requestReview(in: scene)
+        if #available(iOS 14.0, *) {
+            if let scene = UIApplication.shared.connectedScenes.first(where: { $0.activationState == .foregroundActive }) as? UIWindowScene {
+                DispatchQueue.main.async {
+                    requestReview(in: scene)
+                }
             }
         }
     }
